16de71d495453e943b4c0805f1eba425094f1be7
Justin Kleiber <jkleiber8@gmail.com>
2019-08-29 16:28:15 -0500

    Makes straight lines all be some length

--- src/gtddp_target_node.cpp
+++ src/gtddp_target_node.cpp
@@ -26,7 +26,7 @@ double current_time = 0.0;
 
 //TargetTrajectory *target_traj = new StraightLine(0, 0, 5, 10.0);    //fly up 5 meters
 //TargetTrajectory *target_traj = new StraightLine(0, 5, 0, 100.0);    //fly on y axis 5 meters
-//TargetTrajectory *target_traj = new StraightLine(3, 0, 0, 10.0);    //fly forward 5 meters
+//TargetTrajectory *target_traj = new StraightLine(5, 0, 1, 10.0);    //fly forward 5 meters
 //TargetTrajectory *target_traj = new StraightLine(0, 5, 5, 1.5);    //fly up 5 meters and sideways 5 meters
 //TargetTrajectory *target_traj = new SpinAround(2, 40);
 TargetTrajectory *target_traj = new FigureEight(1.6, 1.6, 1.0, 0.2);  //fly a figure eight at 0.8 rad/sec
