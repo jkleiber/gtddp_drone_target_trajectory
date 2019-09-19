2c52a370541000d48f6e90400ce58821554da847
Justin Kleiber <jkleiber8@gmail.com>
2019-09-19 12:14:22 -0500

    Reduces figure 8 size

--- src/gtddp_target_node.cpp
+++ src/gtddp_target_node.cpp
@@ -29,7 +29,7 @@ double current_time = 0.0;
 //TargetTrajectory *target_traj = new StraightLine(5, 0, 1, 10.0);    //fly forward 5 meters
 //TargetTrajectory *target_traj = new StraightLine(0, 5, 5, 1.5);    //fly up 5 meters and sideways 5 meters
 //TargetTrajectory *target_traj = new SpinAround(2, 40);
-TargetTrajectory *target_traj = new FigureEight(1.6, 1.6, 1.0, 0.2);  //fly a figure eight at 0.8 rad/sec
+TargetTrajectory *target_traj = new FigureEight(0.9, 0.9, 1.0, 0.4);  //fly a figure eight at 0.8 rad/sec
 //TargetTrajectory *target_traj = new InclinedCircle(0.9, 0.8, 0.45, 0.98);
 
 bool target_callback(gtddp_drone_msgs::target::Request &req, gtddp_drone_msgs::target::Response &resp)
@@ -51,11 +51,11 @@ void init_conds_callback(const gtddp_drone_msgs::state_data::ConstPtr& init_cond
 }
 
 /**
- * @brief 
- * 
- * @param argc 
- * @param argv 
- * @return int 
+ * @brief
+ *
+ * @param argc
+ * @param argv
+ * @return int
  */
 int main(int argc, char **argv)
 {
@@ -73,7 +73,7 @@ int main(int argc, char **argv)
 
     //Advertise control output and landing mode
     //ros::Publisher target_pub = target_node.advertise<gtddp_drone_msgs::state_data>(target_node.resolveName("/gtddp_drone_target_trajectory/target_state"), MAX_BUFFER);
-    
+
     //Subscribe to any initial conditions updates from the main controller
     ros::Subscriber init_sub = target_node.subscribe(target_node.resolveName("/gtddp_drone/initial_conditions"), MAX_BUFFER, &init_conds_callback);
 
